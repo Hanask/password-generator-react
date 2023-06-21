65e9e7334a8b0196f8a967b4eda9be0923e6c6a5
KHAN, HANA S <hsk2001@hw.ac.uk>
2023-06-21 12:32:48 +0400

    Add password field

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -4,13 +4,22 @@ import { FiRefreshCw } from "react-icons/fi";
 const MainPage = () => {
   return (
     <div>
-      <h1 className="text-4xl text-center font-medium">Password Generator</h1>
-      <button className=" bg-indigo-700 text-indigo-100 px-4 py-4 rounded-lg hover:bg-indigo-500 shadow-lg inline-flex ">
-        Generate Password
-        <span className="ml-4">
-          <FiRefreshCw size={28} />
-        </span>
-      </button>
+      <div>
+        <h1 className="text-4xl text-center font-medium">Password Generator</h1>
+        <button className=" bg-indigo-700 text-indigo-100 px-4 py-4 rounded-lg hover:bg-indigo-500 shadow-lg inline-flex ">
+          Generate Password
+          <span className="ml-4">
+            <FiRefreshCw size={28} />
+          </span>
+        </button>
+      </div>
+      <div>
+        <input
+          className=" w-3/4 border-indigo-700 text-indigo-500 px-4 py-4 rounded-md border-2 mt-4 text-lg"
+          readOnly
+          placeholder="Password:"
+        />
+      </div>
     </div>
   );
 };
