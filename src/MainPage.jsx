fad3feb9d3c6c030c4a8830cb059f93adc6c0a9d
KHAN, HANA S <hsk2001@hw.ac.uk>
2023-06-21 11:42:28 +0400

    Add generate password button

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { FiRefreshCw } from "react-icons/fi";
 
 const MainPage = () => {
-    return (
-        <h1 className="text-4xl text-center font-medium">Password Generator</h1>
-    )
-}
+  return (
+    <div>
+      <h1 className="text-4xl text-center font-medium">Password Generator</h1>
+      <button className=" bg-indigo-700 text-indigo-100 px-4 py-4 rounded-lg hover:bg-indigo-500 shadow-lg inline-flex ">
+        Generate Password
+        <span className="ml-4">
+          <FiRefreshCw size={28} />
+        </span>
+      </button>
+    </div>
+  );
+};
 
 export default MainPage;
